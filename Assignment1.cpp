0d65a4b1431112fc00f4049e5484cab0c2d9c543
NDesai18 <60348023+NDesai18@users.noreply.github.com>
2020-01-29 21:36:49 +0000

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -13,81 +13,94 @@ double deltaE(int Z, int n_i, int n_f) // returns the photon energy
 double eVToJoules(double E) //converts energies from eV to Joules
 {
 	double j;
-	j = E / 1.6;
+	j = E / 1.6*pow(10,-16);
 	return j;
 }
 
-int main(){
-bool run{ true };
-std::string temp;
-std::string temp1;
-std::string temp2;
 
-while (run == true) 
-{
-	int initial_quantum_number{};
-	int final_quantum_number{};
-	int atomic_number{};
-	double photon_energy{};
-	bool quantum_numbers_valid{ false };
-	bool units_valid{ false };
+int main() {
+	bool run{ true };
+	bool string_okay{};
+	std::string temp;
 
-	std::cout << "Enter the atomic number, Z, of the atom" << std::endl;
-	std::getline(std::cin, temp);
-	atomic_number = std::stoi(temp);
-	if (atomic_number >= 0) {
-		std::cout << "Z =" << atomic_number << std::endl;
-	}
-	else { 
-		continue;  //User cannot continue unless they enter a positive value of Z
-	}
-
-	while (quantum_numbers_valid == false) 
+	while (run == true)
 	{
-		std::cout << "Enter the initial quantum number, n_i" << std::endl;
-		std::getline(std::cin, temp1);
-		initial_quantum_number = std::stoi(temp1);
-		std::cout << "Enter the final quantum number, n_f" << std::endl;
-		std::getline(std::cin, temp2);
-		final_quantum_number = std::stoi(temp2);
+		int initial_quantum_number{};
+		int final_quantum_number{};
+		int atomic_number{};
+		double photon_energy{};
+		bool quantum_numbers_valid{ false };
+		bool units_valid{ false };
 
-		if (initial_quantum_number <= final_quantum_number) {
-			std::cout << initial_quantum_number << ",";
-			std::cout << final_quantum_number << std::endl;
-			std::cout << "Error, the inital quantum number must be larger than the final quantum number" << std::endl;
-			std::cin.clear();
-			std::cin.ignore();
-			continue; //Again, user cannot continue unless their inputs are Physically allowed
+		std::cout << "Enter the atomic number, Z, of the atom" << std::endl;
+		try {
+			std::getline(std::cin, temp);
+			atomic_number = std::stoi(temp);
+		}
+		catch (const std::invalid_argument) {
+			std::cout << "Don't put in characters that aren't numbers" << std::endl;
+			return 0; // This ensures the program doesn't get stuck if the user accidentally enters a character that's not a number
+		}
+
+		if (atomic_number >= 0) {
+			std::cout << "Z =" << atomic_number << std::endl;
 		}
 		else {
-			std::cout << "The initial quantum number is " << initial_quantum_number << "and the final quantum number is " << final_quantum_number << std::endl;
-			quantum_numbers_valid = true;
+			continue;  //User cannot continue unless they enter a positive value of Z
 		}
+		while (quantum_numbers_valid == false)
+		{
+			std::cout << "Enter the initial quantum number, n_i" << std::endl;
+			try {
+				std::getline(std::cin, temp);
+				initial_quantum_number = std::stoi(temp);
+			}
+			catch (const std::invalid_argument) {
+				std::cout << "Don't put in characters that aren't numbers" << std::endl;
+				return 0; // This ensures the program doesn't get stuck if the user accidentally enters a character that's not a number
+			}
+			std::cout << "Enter the final quantum number, n_f" << std::endl;
+			try {
+				std::getline(std::cin, temp);
+				final_quantum_number = std::stoi(temp);
+			}
+			catch (const std::invalid_argument) {
+				std::cout << "Don't put in characters that aren't numbers" << std::endl;
+				return 0; // This ensures the program doesn't get stuck if the user accidentally enters a character that's not a number
+			}
 
-	photon_energy = deltaE(atomic_number, initial_quantum_number, final_quantum_number);
+			if (initial_quantum_number > final_quantum_number&& initial_quantum_number > 0 && final_quantum_number > 0) {
+				std::cout << "The initial quantum number is " << initial_quantum_number << " and the final quantum number is " << final_quantum_number << std::endl;
+				quantum_numbers_valid = true;
+			}
+			else {
+				continue; //Again, user cannot continue unless their inputs are physically allowed
+			}
+		}
+		photon_energy = deltaE(atomic_number, initial_quantum_number, final_quantum_number);
 
-	while (units_valid == false)
-	{
-		std::cout << "Would you like the energy in eV (e) or Joules (j)?" << std::endl;
-		std::string units;
-		std::cin >> units;
-		if (units == "e") {
-			std::cout << "The photon energy is " << std::setprecision(3) << photon_energy << "eV" << std::endl;
-			units_valid = true;
-		}
-		else if (units == "j") {
-			photon_energy = eVToJoules(photon_energy);
-			std::cout << "The photon energy is " << std::setprecision(3) << photon_energy << "x10^-16 J" << std::endl;
-			units_valid = true;
-		}
-		else {
-			continue;
+		while (units_valid == false)
+		{
+			std::cout << "Would you like the energy in eV (e) or Joules (j)?" << std::endl;
+			std::string units;
+			std::cin >> units;
+			if (units == "e") {
+				std::cout << "The photon energy is " << std::setprecision(3) << photon_energy << "eV" << std::endl;
+				units_valid = true;
+			}
+			else if (units == "j") {
+				photon_energy = eVToJoules(photon_energy);
+				std::cout << "The photon energy is " << std::setprecision(3) << photon_energy << "J" << std::endl;
+				units_valid = true;
+			}
+			else {
+				continue;
+			}
 		}
 		std::cout << "Press y to run the program again, or any other key to exit" << std::endl;
 		std::string yesno;
 		std::cin >> yesno;
 		if (yesno == "y") {
-			std::cin.clear();
 			std::cin.ignore();
 			continue;
 		}
@@ -95,7 +108,5 @@ while (run == true)
 			run = false;
 		}
 	}
-}
-}
-return 0;
+	return 0;
 }
